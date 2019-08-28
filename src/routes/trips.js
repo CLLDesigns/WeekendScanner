ba84eff806c70a304df1339016e16546229571cd
CLLDesigns <chrislukelyndon@gmail.com>
2019-08-28 22:03:17 +1000

    Adding requirement for trip dates to trips GET route

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 var trips = require('../service');
 
-// GET /api/trips
+// GET /api/trips?departureDate=YYYY-MM-DD&returnDate=YYYY-MM-DD
 router.route("/trips")
   .get((request, response) => {
-    trips.getFiveCheapestTrips("2019-09-10", "2019-09-15")
-      .then(res => response.json(res))
-      .catch((e) => console.log("error", e));
+    if(Object.entries(request.query).length === 0 && request.query.constructor === Object) {
+      response.status(400).send({ error: 'No dates provided. This endpoint requires a departureDate and returnDate.' })
+    } else {
+      trips.getFiveCheapestTrips(request.query.departureDate, request.query.returnDate)
+        .then(res => response.json(res))
+        .catch((e) => console.log("error", e));
+    }
   })
 
 module.exports = router;
